0f84781e895cf41ae78444a6c3c6a8ff9e660c37
Scott D'Antuono <scottd26@gmail.com>
2019-03-16 16:06:02 -0400

    divisors

---
+++ learn_python/divisors.py
@@ -0,0 +1,14 @@
+range = range(2,11)
+
+
+user_input = input("Pick a number between 2 and 11: ")
+int(user_input)
+
+divisors = set()
+
+
+for elem in range:
+    if user_input / elem == 0:
+        divisors.add(elem)
+
+print(divisors)
--- learn_python/evan.py
+++ learn_python/evan.py
@@ -25,55 +25,29 @@ chatnicks = {
         ('evan','realnflfans'):'gopackgo'
         }
 
-'''
-1. make set of people with nicknames
-2. for each key in chats,
-     pick out value for key
-     for each item in value
-       check if item is in set of people with nicknames
-       if (yes) {
-
-       } else {
-
-       }s
-
-'''
+# create list of chatnames
+# loop through chats names,
+#     for name in set
+#         if in chat nicknames
+#             do nothing
+#         elif in chat nicknames
+#             add to dictionary
 
 
-names_in_chat= set()    # creates empty set
+chatnames =  set()
+ppl_without_names =  dict()
 
-for channel_members in chats.values():  #loops chats and returns the values as channel_members
-    for name in channel_members:
-        names_in_chat.add(name)  # loops chats values and adds them to names_in_chat
+for names in chats.keys():
+    chatnames.add(names)
 
-all_chats = set()
+def check_for_nicks(x):
+    for x in chats.values():
+        if x in chatnicks.keys():
+            print("nothing")
+        else:
+            ppl_without_names[x] = "chats.values()"
 
-for chat_name in chats.keys():
-    all_chats.add(chat_name)
-# print(names_in_chat)
+for x in chatnames:
+    check_for_nicks(x)
 
-ppl_with_nicks = set()      # creates empty set
-
-for person in chatnicks.keys():   # loops the chat nicknames set and gets the keys
-    ppl_with_nicks.add(person[0])    #  adds only the first item of the key to ppl_with_nicks
-
-# print(ppl_with_nicks)
-
-ppl_wo_nicks = {} #creats empty dictionary
-
-# check chats for names
-
-# check values of chats to see if member is person of
-
-# for members in chat.values()
-
-# function to check for nickname
-
-def check_for_nick(x):
-    if chatnicks.get(person[0]) == x:
-        print("skip, has nickname")
-    else:
-        ppl_wo_nicks["x"] = chatnicks.keys()
-
-check_for_nick("scott")
-print(ppl_wo_nicks)
+print(ppl_without_names)
