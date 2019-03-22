34de288a8409d78e94d6a53ce5d59c07b73954d0
Scott D'Antuono <scottd26@gmail.com>
2019-03-22 16:17:41 -0400

    list comprehension practice

---
+++ learn_python/list_comp.py
@@ -0,0 +1,12 @@
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+b = [num for num in a if num % 2 == 0]
+# b = list()
+#
+# for num in a:
+#     if num % 2 == 0:
+#         b.append(num)
+
+# seems like in this concept, the first thing is what you want and then you add conditions based on that
+# resource https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
+
+print(b)
