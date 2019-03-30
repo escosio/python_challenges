b9593049a4679d55eb0a3a9f9dd4dd930ea927d1
Scott <scottd@Scotts-Mac-mini.local>
2019-03-29 20:57:05 -0400

    check for primeness with functions

--- learn_python/check_primality_func.py
+++ learn_python/check_primality_func.py
@@ -13,15 +13,20 @@ check_prime(x):
 
 '''
 
-user_num = int(input("Pick a number and i'll tell you if it's prime"))
+import sys
+
+user_num = int(input("Pick a number and i'll tell you if it's prime: "))
 
 potential_divisors = range(2,user_num)
 
 def check_prime(x):
-    for x in potential_divisors:
-        if user_num / x <= 0:
-            pass
-        elif user_num / x > 0:
-            print(str(x) + " number is not prime")
+    if user_num % x > 0:
+        pass
+    elif user_num % x == 0:
+        sys.exit(str(user_num)+ " is prime.")
 
-check_prime(user_num)
+
+for num_in_range in potential_divisors:   # looping through all possible divisors
+    check_prime(num_in_range)
+
+print(str(user_num) + " is not a prime number.")
