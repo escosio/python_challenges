339f4512dd2c8cdef63a3dbffbb42c3ad46c0a5d
Scott D'Antuono <scottd26@gmail.com>
2019-04-29 16:01:09 -0400

    update to accept a string

--- learn_python/reverse_word_order_15.py
+++ learn_python/reverse_word_order_15.py
@@ -1,7 +1,8 @@
 # Write a program (using functions!) that asks the user for a long string containing multiple words.
 # Print back to the user the same string, except with the words in backwards order. For example, say I type the string:
 
-test_string = "my name is Scott"
+# test_string = "my name is Scott"
+test_string = input("Send us a string: ")
 
 test_list = test_string.split()
 
@@ -15,7 +16,7 @@ while index >= 0:
     new_list.append(word)
     index = index - 1
 
-# print(new_list)
+
 str1 = ' '.join(new_list)
 print(str1)
 
