1fd795321025b5045ff2f933fe3d26814caa5b1d
Scott D'Antuono <scottd26@gmail.com>
2019-04-29 15:59:23 -0400

    reverse words in string exercise

---
+++ learn_python/reverse_word_order_15.py
@@ -0,0 +1,29 @@
+# Write a program (using functions!) that asks the user for a long string containing multiple words.
+# Print back to the user the same string, except with the words in backwards order. For example, say I type the string:
+
+test_string = "my name is Scott"
+
+test_list = test_string.split()
+
+index = len(test_list) - 1
+
+new_list = list()
+
+
+while index >= 0:
+    word = str(test_list[index])
+    new_list.append(word)
+    index = index - 1
+
+# print(new_list)
+str1 = ' '.join(new_list)
+print(str1)
+
+
+'''
+get the highest value
+write func to print the last value in list
+    then decrease the index by 1 until index is >= 0
+
+
+'''
