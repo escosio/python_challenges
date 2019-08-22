cc7e5edd390eaec1d15b6cd4caac80349fcafc74
Scott D'Antuono <scottd26@gmail.com>
2019-08-22 16:31:05 -0400

    latest

--- learn_python/evan.py
+++ learn_python/evan.py
@@ -3,13 +3,13 @@
 # ok i just made up a coding challenge for you
 # lol
 # we've got 2 dictionaries
-
+#
 # chats = {'turds odouls':set(['duncan','evan','scott']),'baseball':set(['scott','danjw','duncan']),'realnflfans':set(['scott','evan'])}
 # chatnicks = {('duncan','baseball'):'yankeeboy',('duncan','turds odouls'):'poop butt',('evan','realnflfans'):'gopackgo'}
-
+#
 # chats gives a chat name and the set of members
 # chatnicks gives a member,chat tuple a nickname
-
+#
 # the challenge! find everyone in chats that doesn't have a nickname. the output should be a dictionary of name to a list of channels that you don't have a nickname in
 # so evan:['turds odouls'], scott:['turds odouls','baseball','realnflfans'] ...
 # (a set would also make sense instead of a list)
@@ -24,6 +24,7 @@ chatnicks = {
         ('duncan','turds odouls'):'poop butt',
         ('evan','realnflfans'):'gopackgo'
         }
+
 
 '''
 1. make set of people with nicknames
